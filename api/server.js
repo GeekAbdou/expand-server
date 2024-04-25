3462799cc20de479f06918d7b2210d4b6d80c9c2
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-25 22:14:21 +0200

    changing to sevrerless

--- api/server.js
+++ api/server.js
@@ -1,20 +1,31 @@
-// See https://github.com/typicode/json-server#module
+// Import the necessary modules
 const jsonServer = require("json-server");
+const fs = require("fs");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Apply middlewares
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/product/:resource/:id/show": "/:resource/:id",
-  })
-);
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+
+// Asynchronously read the contents of db.json
+fs.readFile("../db.json", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading db.json:", err);
+    return;
+  }
+  const db = JSON.parse(data);
+
+  // Set up the router with the parsed JSON data
+  const router = jsonServer.router(db);
+
+  // Use the router
+  server.use(router);
+
+  // Start the server
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+  });
 });
 
 // Export the Server API
