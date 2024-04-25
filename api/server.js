a1c6b011398d93c0727972e8b20268167ccf8f69
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-25 22:01:45 +0200

    editing server POST

--- api/server.js
+++ api/server.js
@@ -1,38 +1,21 @@
-// api/server.js
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const rewrite = require("http-rewrite-middleware").getMiddleware;
 
-const handler = (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  // For rewriting routes
-  const rewriter = rewrite([
-    {
-      from: "^/api/(.*)$",
-      to: "/$1",
-    },
-    {
-      from: "^/product/(.*)/([0-9]+)/show$",
-      to: "/$1/$2",
-    },
-  ]);
-  rewriter(req, res, () => {
-    // If no match, pass request to JSON Server
-    router(req, res);
-  });
-};
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/product/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
-module.exports = handler;
+// Export the Server API
+module.exports = server;
