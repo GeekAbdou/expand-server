d804a9f3c262c85b7f15f2965f7f1ab80f996e66
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-27 05:26:11 +0200

    json server auth

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // Import the necessary modules
 const jsonServer = require("json-server");
-const fs = require("fs");
 const jsonServerAuth = require("json-server-auth");
+const path = require("path");
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
@@ -9,37 +9,28 @@ const middlewares = jsonServer.defaults();
 // Apply middlewares
 server.use(middlewares);
 
-// Asynchronously read the contents of db.json
-fs.readFile("db.json", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading db.json:", err);
-    return;
-  }
-  const db = JSON.parse(data);
+// Set up the JSON Server router
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 
-  // Add URL rewriting middleware
-  server.use(
-    jsonServer.rewriter({
-      "/api/*": "/$1",
-      "/products/:resource/:id/show": "/:resource/:id",
-    })
-  );
+// Apply json-server-auth middleware
+server.db = router.db; // Set server.db before applying json-server-auth
+server.use(jsonServerAuth);
 
-  // Apply json-server-auth middleware
-  server.db = db; // Needed for json-server-auth
-  server.use(jsonServerAuth);
+// Add URL rewriting middleware
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/products/:resource/:id/show": "/:resource/:id",
+  })
+);
 
-  // Set up the router with the parsed JSON data
-  const router = jsonServer.router(db);
+// Use the router
+server.use(router);
 
-  // Use the router
-  server.use(router);
-
-  // Start the server
-  const port = process.env.PORT || 3001;
-  server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);
-  });
+// Start the server
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
 
 // Export the Server API
